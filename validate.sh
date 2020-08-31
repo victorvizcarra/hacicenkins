5ce3e974ca63ada3aa7bbddff140d53ff2d63764
Ismail YENIGUL <ismailyenigul@gmail.com>
2020-08-31 20:36:20 +0300

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/bash
 
 set -e
+
+curl -L -o /usr/local/bin/yq  https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 && chmod 755 /usr/local/bin/yq
+
 pattern="^arn:aws:secretsmanager:\S+:\d+:secret:\S+$"
 echo "exit 1 test"
-(yq r config.yml us-east-2.stage.secrets_from || exit -1) | 
+(/usr/local/bin/yq r config.yml us-east-2.stage.secrets_from || exit -1) | 
 while read -r ENV
 read -r VALUE
 do 
